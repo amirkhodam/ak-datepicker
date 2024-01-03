974e6d81eb71dd1126b1d0a128152fbf2a2a292e
AmirKhodam <77395761+amirkhodam@users.noreply.github.com>
2024-01-04 02:14:19 +0330

    Update development components
    main.ts
    App.vue
    index.html

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { DatePicker } from './components'
+import { ref } from 'vue'
+
+const date = ref('')
+</script>
+
+<template>
+  <div class="w-8 h-8 ak-bg-red-600"></div>
+  <DatePicker v-model:value="date" />
+</template>
+
